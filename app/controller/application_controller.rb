41c29944ea851fe547d2c15e04bdee7c4eea708a
Mutoi01 <pngando9@gmail.com>
2022-09-12 04:15:28 +0300

    first commit

---
+++ app/controller/application_controller.rb
@@ -0,0 +1,51 @@
+class ApplicationController < Sinatra::Base
+  
+  set :default_content_type, 'application/json'
+
+  get '/staffs' do
+       staff = Staff.all
+      staff.to_json
+  end
+
+  get '/ships' do
+    ship = Ship.all
+   ship.to_json
+  end
+
+  post '/staffs' do
+    staff = Staff.create(
+    name:params[:name],
+    age:params[:age],
+    gender:params[:gender],
+    occupation:params[:occupation])
+    staff.to_json
+  end
+
+  post '/ships' do
+    ship = Ship.create(
+    vessel:params[:vessel],
+    built:params[:built],
+    gt:params[:gt],
+    size:params[:size])
+    ship.to_json
+  end
+
+  delete '/ships/:id' do
+    ship = Ship.find(params[:id])
+    ship.destroy
+    ship.to_json
+  end
+
+  delete '/staffs/:id' do
+    staff = Staff.find(params[:id])
+    staff.destroy
+    staff.to_json
+  end
+
+ 
+
+  # get '/' do
+  #   '<h2>Hello <em>World</em>!</h2>'
+  # end
+
+end
