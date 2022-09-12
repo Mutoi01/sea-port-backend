00ef44394cee90d73ea9240ba546e154e06f7a01
Mutoi01 <pngando9@gmail.com>
2022-09-12 04:29:21 +0300

    add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,97 @@
+# puts "seeding data"
+
+
+# staff_data = [
+#   {
+#     name: "Tom"
+#     age: "25"
+#     gender: "M"
+#     occupation: "Manager"
+#   },
+#   {
+#     name: "Dickson"
+#     age: "31"
+#     gender: "M"
+#     occupation: "Manager"
+#   },
+#   {
+#     name: "Harry"
+#     age: "45"
+#     gender: "M"
+#     occupation: "Assiatant manager"
+#   },
+#   {
+#     name: "Jane"
+#     age: "26"
+#     gender: "F"
+#     occupation: "HR"
+#   },
+#   {
+#     name: "Allison"
+#     age: "24"
+#     gender: "F"
+#     occupation: "Financial manager"
+#   },
+#   {
+#     name: "Kimathi"
+#     age: "45"
+#     gender: "F"
+#     occupation: "Financial manager"
+#   },
+#   {
+#     name: "Alex"
+#     age: "32"
+#     gender: "M"
+#     occupation: "Financial analyst"
+#   }
+# ]
+
+# ship_data = [
+#   {
+#     vessel: "Prelude"
+#     built: "2017"
+#     gt: "499167"
+#     size: "488"
+#   },
+#   {
+#     vessel: "Pioneer"
+#     built: "2014"
+#     gt: "403342"
+#     size: "477"
+#   },
+#   {
+#     vessel: "Coral"
+#     built: "2021"
+#     gt: "499167"
+#     size: "488"
+#   },
+#   {
+#     vessel: "Dorothy"
+#     built: "2000"
+#     gt: "400100"
+#     size: "385"
+#   },
+#   {
+#     vessel: "Flung"
+#     built: "2005"
+#     gt: "395450"
+#     size: "412"
+#   },
+#   {
+#     vessel: "Jamil"
+#     built: "2002"
+#     gt: "452112"
+#     size: "432"
+#   },
+#   {
+#     vessel: "Alpha"
+#     built: "2011"
+#     gt: "432269"
+#     size: "399"
+#   }
+# ]
+
+# staff_data.each{|s|Staff.create(s)}
+# ship_data.each{|v|Ship.create(v)}
+
+# puts "done" 
